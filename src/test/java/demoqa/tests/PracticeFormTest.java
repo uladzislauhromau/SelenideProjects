e8fd18b3cd8cdba776522fc06ba9fa7a0c7365a3
Uladzislau Hromau <uladzislau.hromau@gmail.com>
2024-04-08 13:46:25 +0300

    add tags

--- src/test/java/demoqa/tests/PracticeFormTest.java
+++ src/test/java/demoqa/tests/PracticeFormTest.java
@@ -1,10 +1,12 @@
 package demoqa.tests;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
 public class PracticeFormTest extends TestBase
 {
-
     @Test
+    @Tag("form")
     void practiceFormTest () {
 
         registrationPage.openPage()
